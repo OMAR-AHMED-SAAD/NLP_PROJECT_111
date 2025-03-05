5a387aeefbb253a64edbc4327b7e9c5177be63c1
karimgamaleldin <karimgamaleldin74@gmail.com>
2025-03-05 12:06:17 +0200

    feat: add new stopwords & perform topic modelling on text - use stopwords from 3 different sources - perform coherance score to find the best number of topics for LDA - perfom LDA using Gensim

--- sentiment2.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-sa = pipeline('text-classification', model='CAMeL-Lab/bert-base-arabic-camelbert-ca-sentiment')
-sentences = ['أنا بخير', 'أنا لست بخير']
-results = sa(sentences)
-print(results)
